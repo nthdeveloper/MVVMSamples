c724105600b0e85361dddbb440ed372d9fd258f2
mkok <mustafa.kok@metus.com>
2018-04-02 20:45:54 +0300

    Command and selection binding sample added.

---
+++ 02_CommandBinding/Utils/MessageBoxHelper.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace CommandBinding.Utils
+{
+    class MessageBoxHelper
+    {
+        public static MessageBoxResult Show(string messageBoxText)
+        {
+            return MessageBox.Show(messageBoxText);
+        }
+
+        public static MessageBoxResult Show(string messageBoxText, string caption)
+        {
+            return MessageBox.Show(messageBoxText, caption);
+        }
+
+        public static MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button)
+        {
+            return MessageBox.Show(messageBoxText, caption, button);
+        }
+
+        public static MessageBoxResult Show(string messageBoxText, string caption, MessageBoxButton button, MessageBoxImage icon)
+        {
+            return MessageBox.Show(messageBoxText, caption, button, icon);
+        }
+    }
+}
